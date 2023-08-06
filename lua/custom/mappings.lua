55dc61a6df29e5de3628f017244cce35b37d1a30
Marius Tomescu <tomescumarius@hotmail.com>
2023-08-06 14:31:21 +0200

    added python lsp

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -14,4 +14,15 @@ mappings.dap = {
   }
 }
 
+mappings.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function ()
+        require('dap-python').test_method()
+      end
+    }
+  }
+}
+
 return mappings
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -10,10 +10,14 @@ local plugins = {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "mypy",
+        "ruff",
+        "black",
         "pyright",
         "clangd",
         "clang-format",
         "codelldb",
+        "debugpy"
       },
     },
   },
@@ -58,6 +62,19 @@ local plugins = {
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, opts)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
     end
   },
 }
